10b7e8e671e7276976d1b9f4d327142fdd106b7b
Catalin Gerea <gereac@gmail.com>
2011-02-01 00:14:36 +0200

    adding empty handlers

---
+++ src/com/gcsf/pcm/handlers/user/AddUserHandler.java
@@ -0,0 +1,41 @@
+package com.gcsf.pcm.handlers.user;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class AddUserHandler extends AbstractHandler implements IHandler {
+	/**
+	 * The constructor.
+	 */
+	public AddUserHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(),
+				"Personal Contact Manager", "Hello, Eclipse world");
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
